1e9d5b968dfcaeccf8e001d9fa8b9d3f7cc704f9
myself <myself@sartoo.com>
2021-11-28 13:21:04 -0700

    add auth to itemtypes controller

--- app/Http/Controllers/ItemTypesController.php
+++ app/Http/Controllers/ItemTypesController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Auth;
 
 class ItemTypesController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index','show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
