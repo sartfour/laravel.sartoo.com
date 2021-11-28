adfac5873397561187061a34b128e46ef09fbec1
myself <myself@sartoo.com>
2021-11-28 13:02:18 -0700

    add auth to items controller

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -11,6 +11,11 @@ use Illuminate\Support\Facades\Auth;
 
 class ItemsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index','show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
