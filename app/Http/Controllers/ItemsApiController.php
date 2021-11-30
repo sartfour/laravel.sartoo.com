92e7a43776ecc8d5c542dbea1ad33a3276adea22
myself <myself@sartoo.com>
2021-11-29 19:02:18 -0700

    additional api controller update

--- app/Http/Controllers/ItemsApiController.php
+++ app/Http/Controllers/ItemsApiController.php
@@ -16,23 +16,12 @@ class ItemsApiController extends Controller
         $this->middleware('auth', ['except' => ['index','show']]);
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $items = Item::select('items.*')->join('item_types', 'items.item_type_id', '=', 'item_types.id')->orderBy('item_types.singular_name')->orderBy('name')->get();
         return $items;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreItemTypeRequest  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreItemRequest $request)
     {
 
@@ -42,39 +31,14 @@ class ItemsApiController extends Controller
             'user_id' => Auth::id(),
         ]);
 
-        return redirect()->action([self::class, 'index'])->with('success', 'A new item was created.');
+        return response()->json($item, 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\ItemType  $itemType
-     * @return \Illuminate\Http\Response
-     */
     public function show(Item $item)
     {
         return $item;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\ItemType  $itemType
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Item $item)
-    {
-        $itemTypes = ItemType::all()->sortBy('singular_name');
-        return view('item.edit', compact('item', 'itemTypes'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateItemTypeRequest  $request
-     * @param  \App\Models\ItemType  $itemType
-     * @return \Illuminate\Http\Response
-     */
     public function update(UpdateItemRequest $request, Item $item)
     {
         $item->update([
@@ -82,19 +46,13 @@ class ItemsApiController extends Controller
             'item_type_id' => $request->item_type_id
         ]);
 
-        return redirect()->action([self::class, 'index'])->with('success', 'Item has been updated.');
+        return response()->json($item, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\ItemType  $itemType
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Item $item)
+    public function delete(Item $item)
     {
         $item->delete();
 
-        return redirect()->action([self::class, 'index'])->with('success', 'Item has been deleted.');
+        return response()->json(null, 204);
     }
 }
